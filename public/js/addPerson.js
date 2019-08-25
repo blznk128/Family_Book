b6a0db2d54cc785c47173aa58c0c8b99cdda16ff
Benny Borunda <blznk128@yahoo.com>
2019-08-24 21:33:32 -0600

    added more routes

--- public/js/addPerson.js
+++ public/js/addPerson.js
@@ -1,21 +1,19 @@
-$(document).ready(function() {
+$(document).ready(() => {
 
 let firstName = $("#firstName");
 
-$("#submit").on("click", function() {
+$("#submit").on("click",() => {
     event.preventDefault();
     let newPerson = {
         first_Name: firstName.val()
     };
-    console.log(firstName)
+    console.log("this is firstName: " +firstName)
     addPerson(newPerson);
 });
 
 function addPerson(Person) {
-    $.post("/api/persons", Person, function() {
-        console.log("this is post: " +Person)
+    $.post("/api/persons", Person,() => {
+        console.log("this is post: " + Person.first_Name)
     })
 }
-
-
 });
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,14 +1,21 @@
 var db = require("../models");
 
-module.exports = function (app) {
-    app.post("/api/persons", function(req, res) {
+module.exports = (app) => {
+    app.post("/api/persons",(req, res) => {
         console.log(req.body);
         db.Person.create({
             first_Name: req.body.first_Name
         })
-        .then(function(dbPerson){
+        .then((dbPerson) => {
+            console.log("this is dbPerson " + dbPerson.first_Name)
             res.json(dbPerson)
-            console.log("this is dbPost " + dbPerson)
         })
-    })
+    });
+
+    app.get("/api/persons/", (req, res) => {
+        db.Person.findAll({})
+            .then((dbPerson)=> {
+                res.json(dbPerson)
+            });
+    });
 }
