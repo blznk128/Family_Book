1cb0ef7c087f5791792f6410eba4c2419c61b975
Benny Borunda <blznk128@yahoo.com>
2019-08-26 19:31:41 -0600

    added routes for getting database

--- public/js/people.js
+++ public/js/people.js
@@ -1,5 +1,28 @@
+let people;
+let peopleContainer = $("#people");
+
+//button to go to add person page
 $("#toAddPerson").on("click", () => {
     event.preventDefault();
     window.location.href = "/addPerson";
 });
 
+//This gets everybody from the database
+function getPeople() {
+    $.get("/api/persons/", (data) => {
+        people = data;
+        let postsToAdd = []
+        for (let i = 0; i < people.length; i++) {
+            postsToAdd.push("<button>" + people[i].first_Name + "</button>" + " ");
+          };
+        peopleContainer.append(postsToAdd);
+    });
+};
+
+getPeople();
+
+function addButtons(post) {
+    let button = $("<button>")
+    peopleContainer.append(button)
+    // peopleContainer.append(post.first_Name)
+}
