cb7eaab95f3fed6fd07f0e2ac77bfd68bcf86a3c
Benny Borunda <blznk128@yahoo.com>
2019-08-27 21:45:03 -0600

    finally got foreign key working

--- models/people.js
+++ models/people.js
@@ -1,11 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
     let Person = sequelize.define("Person", {
-        // id: {
-        //     type: DataTypes.UUID,
-        //     primaryKey: true,
-        //     defaultValue: DataTypes.UUIDV4,
-        //     allowNull: false
-        // },
         first_Name: {
             type: DataTypes.STRING,
             allowNull: false
--- public/js/addPerson.js
+++ public/js/addPerson.js
@@ -10,13 +10,12 @@ $("#submit").on("click",() => {
         first_Name: firstName.val(),
         last_Name: lastName.val(),
     };
-    console.log("submit person: " + newPerson.id)
-    let newKid = {
-        kid_Name: kidName.val(),
-    }
+    
     console.log("submit kid: " + newPerson.first_Name)
-    addPerson(newPerson);
-    addKid(newKid);
+    // addPerson(newPerson);
+    trialOne(newPerson)
+    // addKid(newKid);
+    // getPersonId(newKid)
     
 });
 
@@ -29,6 +28,46 @@ function addPerson(Person) {
 
 function addKid(Kid) {
     $.post("/api/kid", Kid, () => {
-        console.log("this is kidname: " + Kid.kid_Name)
+        // console.log("this is kidname: " + Kid.kid_Name)
+    })
+}
+
+function getPersonId(information) {
+    $.ajax({
+        method: "GET",
+        url: "/api/persons",
+        data: information,
+    })
+        .then(addKid(information))
+}
+
+function trialOne(Person) {
+    $.post("/api/persons", Person,() => {
+        console.log("this is post: " + Person.first_Name);
+    })
+    .then(function(information) {
+        $.ajax({
+            method: "GET",
+            url: "/api/persons",
+            data: information,
+        })
+            .then(function() {
+                
+                let kidName = $("#kidName");
+                let firstName = $("#firstName");
+                let lastName = $("#lastName");
+                
+                let newPerson = {
+                    first_Name: firstName.val(),
+                    last_Name: lastName.val(),
+                };
+                console.log("maybe three: ", information.id)
+                console.log("this is twice now: " , information.first_Name)
+                let newKid = {
+                    kid_Name: kidName.val(),
+                    PersonId: information.id
+                }
+                addKid(newKid)
+            })
     })
 }
