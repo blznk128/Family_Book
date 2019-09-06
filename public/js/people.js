cb663db6930731a621355b51ed9570d8c5ceb4de
Benny Borunda <blznk128@yahoo.com>
2019-09-05 21:47:41 -0600

    added buttons

--- public/js/people.js
+++ public/js/people.js
@@ -1,18 +1,14 @@
+$(document).ready(function() {
 let people;
 let kid;
 let peopleContainer = $("#people");
-let kidContainer = $("#kids");
+let kidContainer = $(".kids");
 
 //button to go to add person page
 $("#toAddPerson").on("click", () => {
     event.preventDefault();
     window.location.href = "/addPerson";
 });
-
-
-function heyMan() {
-  alert("what up dawg");
-}
 
 //This gets everybody from the database
 function getPeople(author) {
@@ -24,14 +20,43 @@ function getPeople(author) {
         people = data;
         let peopleToAdd = []
         for (let i = 0; i < people.length; i++) {
-            peopleToAdd.push("<button id =" + people[i].Person.first_Name + ">" + people[i].Person.first_Name + "  " + people[i].Person.last_Name);
+            // peopleToAdd.push("<button id =" + people[i].Person.first_Name + ">" + people[i].Person.first_Name + "  " + people[i].Person.last_Name);
+            // peopleToAdd.push("<button class =" + "penguin" + ">" + people[i].Person.first_Name + "  " + people[i].Person.last_Name);
+            peopleToAdd.push("<button id =" + "penguin" +  people[i].Person.id + ">" + people[i].Person.first_Name + "  " + people[i].Person.last_Name);
             console.log("this is person: " + people[i].Person.first_Name)
             console.log("this is kid: " + people[i].kid_Name)
+            $(`#penguin${people[i].Person.first_Name}`).on("click",function() {
+              console.log("click worked", this.first_Name)
+            })
           };
-        peopleContainer.append(peopleToAdd);
+          peopleContainer.append(peopleToAdd);
+          console.log("this is peopletoadd: ",peopleToAdd.kid_Name )
+          
+          
+          
+        
     })
 };
 getPeople();
+
+function getKids() {
+  $(`#penguin${this.Person}`).on("click",function() {
+    console.log("click worked", this.first_Name)
+  })
+}
+
+
+})
+
+
+
+
+
+
+
+
+
+
 
 
 // function getKid() {
